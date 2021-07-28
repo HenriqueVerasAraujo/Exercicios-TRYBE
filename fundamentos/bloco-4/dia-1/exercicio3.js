4e98c07eefd80598b67156533df1788b26701cbd
Henrique_Veras <theveras1@gmail.com>
2021-07-28 17:44:47 -0300

    exercício 3 da aula

---
+++ fundamentos/bloco-4/dia-1/exercicio3.js
@@ -0,0 +1,13 @@
+let a = 40
+let b = 50
+let c = 30
+
+if (a > b && a > c) {
+    console.log("'a' é maior que 'b' e 'c'.");
+}
+else if (b > a && b > c) {
+    console.log("b é maior que 'a' e 'c'.")
+}
+else {
+    console.log("'c' é maior que 'a' e 'b'.")
+}
