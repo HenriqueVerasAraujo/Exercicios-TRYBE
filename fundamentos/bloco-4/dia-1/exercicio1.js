d004bcfc1122aa6c28cc65cd837f73f95a000803
Henrique_Veras <theveras1@gmail.com>
2021-07-28 17:28:55 -0300

    exercício 1 da aula

---
+++ fundamentos/bloco-4/dia-1/exercicio1.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 5;
+let adição = a + b;
+console.log(adição);
+
+let subitração = a - b;
+console.log(subitração);
+
+let multiplicação = a * b;
+console.log(multiplicação);
+
+let divisão = a / b;
+console.log(divisão);
+
+let módulo = a % b;
+console.log(módulo);
