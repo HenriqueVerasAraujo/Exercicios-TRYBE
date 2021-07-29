194b49f606585991efb89bb0bc456d8cb15954c1
Henrique_Veras <theveras1@gmail.com>
2021-07-29 18:02:58 -0300

    arquivo de pratica da aula

---
+++ fundamentos/bloco-4/dia-2/pratica.js
@@ -0,0 +1,23 @@
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// menu.push("Contato");
+// let menuServices = menu[4] ;
+// console.log(menuServices);
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// for (let index = 0; index <groceryList.length; index += 1) {
+//     console.log(groceryList[index])
+// }
+
+
+let carros= [1,2,3,4,5];
+for(let carros of carros) {
+  console.log(carro);
+}
+
+// resultado: 
+//1
+//2
+//3
+//4
+//5
