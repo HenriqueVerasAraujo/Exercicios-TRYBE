fc2cabd97688e8579c2c49dfd8db528d1907aa91
Henrique_Veras <theveras1@gmail.com>
2021-07-30 18:34:29 -0300

    Exercicio 1 da aula 4.3

---
+++ fundamentos/bloco-4/dia-3/exercício1.js
@@ -0,0 +1,9 @@
+let colunasAsterisco =""
+let varN = 9
+
+for (let index = 1; index <= varN; index += 1) {
+   colunasAsterisco += "*" + " "
+}
+for (let index2 = 1; index2<= varN; index2 += 1){
+    console.log(colunasAsterisco);
+}
