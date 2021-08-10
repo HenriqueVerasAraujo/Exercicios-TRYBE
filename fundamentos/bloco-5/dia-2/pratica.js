239a72a0ac93f57c8d12f287e50d189953365a7c
Henrique_Veras <theveras1@gmail.com>
2021-08-10 16:30:15 -0300

    Exercícios da aula 5.2, parte de criação de elementos

---
+++ fundamentos/bloco-5/dia-2/pratica.js
@@ -0,0 +1,7 @@
+// const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+
+// const pai = elementoOndeVoceEsta.parentElement;
+//     pai.style.color = 'red';
+
+// const primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild;
+//     primeiroFilhoDoFilho.innerText = 'primeiroFilhoDoFilho';
