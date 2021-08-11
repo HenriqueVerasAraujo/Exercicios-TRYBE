5f034b079c5cb51bbc7372f48e4deaad88c858bd
Henrique_Veras <theveras1@gmail.com>
2021-08-11 15:15:42 -0300

    Prática da aula 5.3, refeito

--- fundamentos/bloco-5/dia-3/pratica/main.js
+++ fundamentos/bloco-5/dia-3/pratica/main.js
@@ -1,51 +1,63 @@
-//Constants//
 const firstLi = document.getElementById('firstLi');
 const secondLi = document.getElementById('secondLi');
 const thirdLi = document.getElementById('thirdLi');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
-//Event Listeners//
-firstLi.addEventListener('mouseover', addTech);
-secondLi.addEventListener('mouseover', addTech);
-thirdLi.addEventListener('mouseover', addTech);
-input.addEventListener('input',subText);
-myWebpage.addEventListener('dblclick', redirect);
+
+
+
+//EVENTS//
+firstLi.addEventListener('mouseover', addtech);
+secondLi.addEventListener('mouseover', addtech);
+thirdLi.addEventListener('mouseover', addtech);
+
+input.addEventListener('input', typeTech);
+
+myWebpage.addEventListener('dblclick', redirectTrybefy);
+
 myWebpage.addEventListener('mouseover', colorChange);
-myWebpage.addEventListener('mouseout', coloroff);
+myWebpage.addEventListener('mouseout', colorChangeBack);
 
-//Functions//
-function addTech(e) {
-    let previousTech = document.getElementsByClassName('tech')[0];
-    previousTech.classList.remove('tech');
-    e.target.className = 'tech';
+
+//FUNCTIONS//
+function addtech(e) {
+   let previusTech = document.getElementsByClassName('tech')[0];
+   previusTech.removeAttribute('class','tech');
+   e.target.className = 'tech';
 }
 
-function subText(event) {
-    let previousTech2 = document.getElementsByClassName('tech')[0];
-    previousTech2.innerText = event.target.value;
+function typeTech(e) {
+    let newText = e.target.value
+    let previusTech = document.getElementsByClassName('tech')[0];
+    previusTech.innerText = newText;
 }
 
-function redirect() {
-    window.location.replace("https://www.youtube.com/");
+function redirectTrybefy() {
+    window.location.assign("https://www.youtube.com/");
 }
-
-function colorChange(e) {
+function colorChange(e){
     e.target.style.color = "red";
 }
-function coloroff(e) {
-    e.target.style.color = "white";
+function colorChangeBack(e){
+    e.target.removeAttribute("style");
 }
-
 /*
  Copie esse arquivo e edite apenas ele;
+
  Crie uma função que adicione a classe 'tech' ao elemento selecionado;
 1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+
  Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 com a classe 'tech';
+
+
  Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
 1. Que tal redirecionar para seu portifólio?
+
+
  Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 a cor do mesmo;
 
@@ -64,3 +76,52 @@ firstLi.addEventListener('dblclick', resetText);
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
 // event.target na nossa função retornará o objeto 'firstLi'.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
